5d2ce3a15e79df0e545f5b3143cd88a2b7fe4046
raja venkata ganesh tadiboina <tadiboinaganesh4@gmail.com>
2026-05-19 01:37:55 +0530

    fixed script

--- script.js
+++ script.js
@@ -4,43 +4,40 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 var marker;
 let pathCoordinates = [];
-
 let polyline;
+
 function loadLocation(){
+  fetch("https://gps-tracking-website-production.up.railway.app/location")
+  .then(res=>res.json())
+  .then(data=>{
 
-fetch("https://gps-tracking-website-production.up.railway.app/location")
+    pathCoordinates.push([data.lat,data.lng]);
 
-.then(res=>res.json())
+    if(polyline){
+      map.removeLayer(polyline);
+    }
 
-.then(data=>{
+    polyline = L.polyline(pathCoordinates).addTo(map);
 
-document.getElementById("coords").innerHTML =
-`Latitude: ${data.lat} Longitude: ${data.lng}`;
+    document.getElementById("coords").innerHTML =
+    `Latitude: ${data.lat} Longitude: ${data.lng}`;
 
-document.getElementById("speed").innerHTML =
-`Speed: ${data.speed} km/h`;
+    document.getElementById("speed").innerHTML =
+    `Speed: ${data.speed} km/h`;
 
-if(marker){
+    if(marker){
+      marker.setLatLng([data.lat,data.lng]);
+    }
+    else{
+      marker = L.marker([data.lat,data.lng])
+      .addTo(map)
+      .bindPopup("Live Vehicle")
+      .openPopup();
+    }
 
-marker.setLatLng([data.lat,data.lng]);
-
+    map.setView([data.lat,data.lng],13);
+  });
 }
 
-else{
-
-marker = L.marker([data.lat,data.lng])
-
-.addTo(map)
-
-.bindPopup("Live Vehicle")
-
-.openPopup();
-
-}
-
-map.setView([data.lat,data.lng],13);
-
-});
-
-}
 setInterval(loadLocation,5000);
+loadLocation();
