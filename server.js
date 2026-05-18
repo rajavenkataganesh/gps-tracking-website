0e6394afa2b8bc08492e6b3d757eda984c4d7453
raja venkata ganesh tadiboina <tadiboinaganesh4@gmail.com>
2026-05-19 00:53:41 +0530

    fixed port for vercel

--- server.js
+++ server.js
@@ -5,35 +5,28 @@ const app = express();
 
 app.use(cors());
 
+// Serve index.html
+app.use(express.static(__dirname));
+
 let vehicleLocation = {
-lat:17.3850,
-lng:78.4867,
-speed:45
+  lat: 17.3850,
+  lng: 78.4867,
+  speed: 45
 };
 
-app.get("/location",(req,res)=>{
-
-res.json(vehicleLocation);
-
+app.get("/location", (req, res) => {
+  res.json(vehicleLocation);
 });
 
-app.get("/update",(req,res)=>{
-
-vehicleLocation.lat =
-parseFloat(req.query.lat);
-
-vehicleLocation.lng =
-parseFloat(req.query.lng);
-
-vehicleLocation.speed =
-req.query.speed || 0;
-
-res.send("Updated");
-
+app.get("/update", (req, res) => {
+  vehicleLocation.lat = parseFloat(req.query.lat);
+  vehicleLocation.lng = parseFloat(req.query.lng);
+  vehicleLocation.speed = req.query.speed || 0;
+  res.send("Updated");
 });
 
-app.listen(3000,()=>{
-
-console.log("Server running");
-
+// Vercel ki dynamic port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
